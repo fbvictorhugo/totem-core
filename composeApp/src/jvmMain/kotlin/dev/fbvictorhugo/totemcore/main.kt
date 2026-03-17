cec495e0c75eb81d4dbc6e6d13ba51e267e64d30
unknown <fb.victorhugo@gmail.com>
2026-03-17 09:22:09 -0300

    Configurations for Main Window Desktop

--- composeApp/src/jvmMain/kotlin/dev/fbvictorhugo/totemcore/main.kt
+++ composeApp/src/jvmMain/kotlin/dev/fbvictorhugo/totemcore/main.kt
@@ -1,12 +1,23 @@
 package dev.fbvictorhugo.totemcore
 
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
+import totemcore.composeapp.generated.resources.Res
+import totemcore.composeapp.generated.resources.app_name
+import totemcore.composeapp.generated.resources.compose_multiplatform
 
 fun main() = application {
+    val windowState = rememberWindowState(placement = WindowPlacement.Maximized)
+
     Window(
         onCloseRequest = ::exitApplication,
-        title = "TotemCore",
+        state = windowState,
+        title = stringResource(Res.string.app_name),
+        icon = painterResource(Res.drawable.compose_multiplatform)
     ) {
         App()
     }
